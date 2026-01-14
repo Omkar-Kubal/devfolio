48b34547341cd53e51720fe1e45bb88508e8137b
omkarkubal <omkarkubal17@gmail.com>
2026-01-14 14:29:54 +0530

    Updated web - 14-01-2025

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -21,13 +21,13 @@ const Hero = () => {
                         />
                     </div>
                     <div className='flex flex-col md:text-xl items-center justify-center text-center px-4 md:px-0 gap-4'>
-                        <p className='text-muted-foreground'>AI-ML Developer <br/>specializing in applied machine learning for cybersecurity systems.</p> 
+                        <p className='text-muted-foreground'>AI-ML Developer <br />specializing in applied machine learning for cybersecurity systems.</p>
                         <p className='text-muted-foreground italic border-l-2 border-muted-foreground/50 pl-2'>Proven in building scalable, security-driven AI solutions</p>
                     </div>
                 </div>
                 <div className='flex items-center justify-center gap-4'>
                     <Link href="https://github.com/Omkar-Kubal" target='_blank'><Button size="sm">Github <IconBrandGithub className="w-4 h-4" /></Button></Link>
-                    <Link href="https://drive.google.com/file/d/1Wb3h1W__QRMzaUaahyd2VazxfeWugGzY/view?usp=sharing" target='_blank'><Button size="sm" variant="outline">Download CV <IconDownload className="w-4 h-4" /></Button></Link>
+                    <Link href="https://drive.google.com/file/d/1bn1K54Qvr4alpsn0Z8FCIzc0uNzSjtWa/view?usp=sharing" target='_blank'><Button size="sm" variant="outline">Download CV <IconDownload className="w-4 h-4" /></Button></Link>
                 </div>
             </div>
         </Background>
